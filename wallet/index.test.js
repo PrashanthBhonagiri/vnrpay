4b9ecdf4741af81ed9eee12aea14c9ffbfbd1ba7
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-06-01 23:39:29 +0530

    sign data and verify signature

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,17 +1,41 @@
 const Wallet = require('./index');
-
+const { verifySignature } = require('../util');
 describe('Wallet', () => {
    let wallet;
    beforeEach(() =>{
        wallet = new Wallet();
    });
-//     it('',() =>{
-
-//    });
    it('has a balance',() =>{
         expect(wallet).toHaveProperty('balance');
    });
    it('has a publicKey',() =>{
         expect(wallet).toHaveProperty('publicKey');
    });
+
+   describe('signing data', () => {
+      const data = 'testing sing';
+
+      it('verifies a signature', () =>{
+         expect(
+            verifySignature({
+               publicKey  : wallet.publicKey,
+               data,
+               signature : wallet.sign(data),
+            })
+         ).toBe(true);
+      });
+
+      it('does not verifyan invalid signature',() =>{
+         expect(
+            verifySignature({
+               publicKey  : wallet.publicKey,
+               data,
+               signature : new Wallet().sign(data),
+            })
+         ).toBe(false);
+
+      });
+   })
+   
+
 });
