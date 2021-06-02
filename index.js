17a345ce915af4099ebeacb3ed3e6cfbefc2903b
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-06-03 00:26:18 +0530

    transaction-pool-map endpoint configured

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/api/transact', (req,res,next) =>{
         }
         transactionPool.setTransaction(transaction);
     
-        console.log('transactionPool = ', transactionPool);
+        // console.log('transactionPool = ', transactionPool);
     
         res.json({ status : true,transaction });
 
@@ -84,6 +84,12 @@ app.post('/api/transact', (req,res,next) =>{
 
 });
 
+app.get('/api/transaction-pool-map',(req,res,next) =>{
+    res.json({
+        status : true,
+        transactionPoolMap : transactionPool.transactionMap
+    })
+});
 
 const syncChains = () => {
     request({url : `${ROOT_NODE_ADDRESS}/api/blocks`}, (error,response, body) =>{
