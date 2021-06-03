916fde5d52ff881f49e43f929aea8416d995619a
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-06-03 13:15:25 +0530

    mine transactions init

---
+++ app/transaction-miner.js
@@ -0,0 +1,23 @@
+class TransactionMiner{
+
+    constructor({ blockchain, transactionPool, wallet, pubsub}){
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.pubsub = pubsub
+    }
+
+    mineTransactions(){
+        //get the transactionPool's valid transaction
+        
+        // generate the miners reward
+
+        //add a block consisting of these transactions to the blockchain
+        
+        //broadcast the updated blockchain
+
+        //clear the pool
+
+    };
+} 
+module.exports = TransactionMiner;
