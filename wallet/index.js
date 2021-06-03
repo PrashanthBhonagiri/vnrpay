ff95d2bb54fcf6e674308f478e2bab26c27f8c44
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2021-06-03 15:08:58 +0530

    calculateBalance()

--- wallet/index.js
+++ wallet/index.js
@@ -21,6 +21,23 @@ class Wallet {
         return new Transaction({senderWallet : this, recipient, amount});
 
     }
+    static calculateBalance({chain,address}) {
+        // console.log(chain.length);
+        let outputsTotal = 0;
+        for(let i=1;i<chain.length;i++ ) {
+            const block = chain[i];
+            // console.log(block);
+
+            for(let transaction of block.data) {
+                const addressOutput = transaction.outputMap[address];
+
+                if(addressOutput) {
+                    outputsTotal += addressOutput;
+                }
+            }
+        }
+        return STARTING_BALANCE + outputsTotal;
+    }
 
 }
 module.exports = Wallet;
